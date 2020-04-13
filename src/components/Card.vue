3dcb864dc140baf4313e881b87abf5bbfc6c45b7
maxm <maxm@mail.taiji.com.cn>
2020-04-13 16:07:45 +0800

    card

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card flex-row shadow">
+    <!-- 传入对象、操作 -->
+    <div class="card-item">
+      <slot>Shwo item here</slot>
+    </div>
+    <div class="card-operator">
+      <span class="iconSpan">
+        <i class="iconfont icon-delete" @click="deleteItem(id)"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    operator: {
+      type: Array,
+      required: false
+    }
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang="less">
+.card {
+  width: 200px;
+  height: 100px;
+}
+.flex-row {
+  display: flex;
+  flex-direction: row;
+}
+.shadow {
+  box-shadow: @shadow-btn;
+}
+</style>
